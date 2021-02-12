e097edbafb736e768b036b8d0187c3e30e3b81cc
Kevin Robert Andrews <kevinrobertandrews@gmail.com>
2021-02-12 07:12:44 -0500

    load a main window

--- app/main.js
+++ app/main.js
@@ -1,7 +1,15 @@
-const { app } = require('electron');
+const { app, BrowserWindow } = require('electron');
 
+// Declare main window in global scope so it doesn't get garbage collected
+let mainWindow = null;
+
+// When the app is ready, load the main browser window
 app.on('ready', () => {
-    console.log('The application is ready.');
+    mainWindow = new BrowserWindow({ width: 800, height: 800, show: false });
+    mainWindow.loadFile(`${__dirname}/index.html`);
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show();
+    });
 });
 
 console.log('Starting up...');
